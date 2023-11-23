c84e86327c6940cd645c5b3af093c6d109571567
dduartevla <deboraduarte@ice.ufjf.br>
2023-11-23 17:34:23 -0300

    Classe MementoJogo

--- src/main/java/MementoJogo.java
+++ src/main/java/MementoJogo.java
@@ -1,2 +1,12 @@
 public class MementoJogo {
+
+    private EstadoJogo estado;
+
+    public MementoJogo(EstadoJogo estado){
+        this.estado = estado;
+    }
+
+    public EstadoJogo getEstado(){
+        return estado;
+    }
 }
