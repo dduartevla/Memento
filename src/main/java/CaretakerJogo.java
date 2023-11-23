70c763d9e3d7b6b7d77757acbbe48da3304d44c0
dduartevla <deboraduarte@ice.ufjf.br>
2023-11-23 17:37:46 -0300

    Classe CaretakerJogo

--- src/main/java/CaretakerJogo.java
+++ src/main/java/CaretakerJogo.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaretakerJogo {
+
+    private List<MementoJogo> historico = new ArrayList<>();
+
+    public void addMemento(MementoJogo memento){
+        historico.add(memento);
+    }
+
+    public MementoJogo getMemento(int indice){
+        if (indice < 0 || indice > historico.size()){
+            throw new IllegalArgumentException("Índice inválido");
+        }
+        return historico.get(indice);
+    }
 }
